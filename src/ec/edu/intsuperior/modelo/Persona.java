7b352fab1e4dc697196e48b401ba61efcec4b5c2
Usuario <Usuario@DESKTOP-M2UDKI8>
2022-07-14 20:12:04 -0500

    Este es el proyecto de base de la materio POO v1.0

---
+++ src/ec/edu/intsuperior/modelo/Persona.java
@@ -0,0 +1,18 @@
+
+package ec.edu.intsuperior.modelo;
+
+import java.util.Date;
+
+
+public class Persona {
+    String nombre1;
+    String nombre2;
+    String apellido1;
+    String apellido2;
+    String cedula;
+    String direccion;
+    Date fechaNacimiento; 
+    String correoElectronico;
+    
+    
+}
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,17 @@
+
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Persona;
+
+
+public class Aplicacion {
+
+    
+    public static void main(String[]args){
+        Persona rojas;
+        SimpleDateFormat formatoFecha=new SimpledateFormat("17/10/1998");
+        
+        
+    
+    }
+}
